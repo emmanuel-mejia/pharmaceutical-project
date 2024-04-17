34c6ecc375cc4fecc4bf097d84713c6a32acc094
“Emmanuel” <emmanuel.mejia.jimenez@gmail.com>
2024-04-16 19:44:09 -0600

    202404161943 Model change
    Classes rename according to Django conventions

--- medicines/models.py
+++ medicines/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from research_facilities.models import ResearchFacility
 
-# Create your models here.
+class Medicine (models.Model):
+    name = models.CharField(max_length=70, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.IntegerField(default=1,null=False)
+    research_facility = models.ForeignKey(ResearchFacility, on_delete=models.CASCADE, related_name='medicines')
+
+    def __str__(self) -> str:
+        return self.name
