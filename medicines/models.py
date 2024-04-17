c8d181144191cff34d5366d5af1d8a594fe50a70
“Emmanuel” <emmanuel.mejia.jimenez@gmail.com>
2024-04-16 19:44:09 -0600

    202404161943 Model change
    Classes rename according to Django conventions

--- medicines/models.py
+++ medicines/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from research_facilities.models import ResearchFacility
 
-class medicines (models.Model):
+class Medicine (models.Model):
     name = models.CharField(max_length=70, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     price = models.IntegerField(default=1,null=False)
--- medicines/views.py
+++ medicines/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from medicines.models import medicine
+from medicines.models import Medicine
 
-def medicines(request):
+def medicines(request): #All functions in Python must be with the request parameter 
+    
     #print('Path medicines')
     #return 'Path medicines'
+   
     #JSON
-    medicines = medicine.objects.all()
-    return JsonResponse(medicines)
+    medicines = Medicine.objects.all()
+
+    result = []
+    for medicine in medicines: 
+        m = {
+            'id': medicine.id,
+            'name': medicine.name,
+            'price': medicine.price,
+        }
+        result.append(m)
+
+
+    return JsonResponse(
+        {
+            'data': result #Dict list
+        }
+    )
